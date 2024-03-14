be1852511fe0640bae8f222a85ad3d044348b31d
3oDoR <nikita_clever@icloud.com>
2024-03-14 13:15:03 +0300

    Created new branch and added packages for MVVM architecture pattern.

--- app/src/main/java/com/practice/schedulefitnessapp/presentation/MainActivity.kt
+++ app/src/main/java/com/practice/schedulefitnessapp/presentation/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.practice.schedulefitnessapp
+package com.practice.schedulefitnessapp.presentation
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.practice.schedulefitnessapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
