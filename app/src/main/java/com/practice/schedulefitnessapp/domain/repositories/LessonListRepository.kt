6bfd3c6c4914951d35ba13b445e72b28557556fa
3oDoR <smirnov2.nv@gmail.com>
2024-03-22 22:42:41 +0300

    Update LessonListRepository

--- app/src/main/java/com/practice/schedulefitnessapp/domain/repositories/LessonListRepository.kt
+++ app/src/main/java/com/practice/schedulefitnessapp/domain/repositories/LessonListRepository.kt
@@ -4,6 +4,8 @@ import com.practice.schedulefitnessapp.domain.items.GeneralItem
 import com.practice.schedulefitnessapp.domain.items.LessonItem
 
 interface LessonListRepository {
+
+    suspend fun getGeneralItem(): GeneralItem
     suspend fun getLessonList(): GeneralItem
 
     fun getLessonItem(lessonItemId: Int): LessonItem
